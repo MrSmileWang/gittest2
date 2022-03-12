a4679264853c1443b569d77f8bbe94d2088fbffd
MrSmileWang <58657746+MrSmileWang@users.noreply.github.com>
2022-03-12 21:32:39 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -9,5 +9,6 @@ public class TestGit {
         System.out.println("master test git4");
         System.out.println("hot-fix test！");
         System.out.println("push test！");
+         System.out.println("pull test！");
     }
 }
